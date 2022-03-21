d609179bcb5a9cfe0639a239d4125a63253007d8
Ayaan Merchant <ayaan.merchant08@gmail.com>
2022-03-20 22:03:48 -0600

    set up sql file to store user information and added a "first time account set-up" function

--- user.py
+++ user.py
@@ -4,12 +4,21 @@ author: Ayaan Merchant
 date-created: 2022-01-07
 '''
 
+import sqlite3
+import pathlib
 import money
 from datetime import date
 from time import sleep
 
 ### VARIABLES ###
-FILE = ##SQL FILE##
+DB_FILE = "user_account.db"
+FIRST_RUN = True
+
+if (pathlib.Path.cwd() / DB_FILE).exists():
+    FIRST_RUN = False # don't create DB if already created
+
+CONNECTION = sqlite3.connect(DB_FILE)
+CURSOR = CONNECTION.cursor()
 
 
 def checkInt(NUM):
@@ -19,6 +28,47 @@ def checkInt(NUM):
     except ValueError:
         NEW_NUM = input("Input must be a number: ")
         return checkInt(NEW_NUM)
+
+## create database and do account set up
+def createDatabase():
+    '''
+    create the database for use account
+    :return: None
+    '''
+
+    # create DB
+    CURSOR.execute('''
+        CREATE TABLE
+            user_account(
+                username TEXT NOT NULL PRIMARY KEY,
+                account_balance STR,
+                transactions BLOB 
+                )
+        ;''') # transactions stores objects but not sure what to store
+
+def getUserInfo():
+    '''
+    when first run, get and store user info in DB
+    :return: None
+    '''
+    print("Thank-you for choosing Budgy")
+    print("Since this is your first time using Budgy, we will have to set up your account \n")
+
+    NAME = input("Your Name (this will be the same as your username): ")
+    CURRENT_BALANCE = input("Your Current Account Balance (this can be updated later): ")
+
+    FIRST_TIME_INFO = (NAME, CURRENT_BALANCE)
+
+    CURSOR.execute('''
+        INSERT INTO
+            user_account(
+                username,
+                account_balance
+                )
+        VALUES(
+            ?, ?  
+            )
+    ;''', FIRST_TIME_INFO)
 
 class User:
     '''
@@ -107,11 +157,12 @@ class User:
         return self.menu()
 
 if __name__ == "__main__":
+    print("Welcome to Budgy! \n")
+
+    if FIRST_RUN:
+        createDatabase()
+        getUserInfo()
+
     USER = User()
-    print("Welcome to Budgy!")
-
-    ### open sql file for user ###
-
-
 
     USER.menu()
